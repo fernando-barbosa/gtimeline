8b3bea7c578bc9d3de40786f86e2a2a629d14e74
Fernando Barbosa <fernando.junior@radixeng.com.br>
2019-01-04 16:56:54 -0200

    Updating list item layout

--- src/components/ListHolderComponent.js
+++ src/components/ListHolderComponent.js
@@ -3,16 +3,24 @@ import { Text, View, Image } from 'react-native';
 import Style from '../styles/listholder';
 
 const ListHolderComponent = (props) => {
-    return(
-        <View style={Style.container}>
-            <Text style={Style.title}>{props.name}</Text>
-            { 
-                props.series.map((child) => (
-                    <Text style={Style.content}>{child.year} - {child.name}</Text>
-                ))
-            }
-        </View>
-    );
+	return(
+    <View style={Style.container}>
+      <Text style={Style.title}>{props.name}</Text>
+      { 
+        props.series.map((child) => (
+          <View style={Style.row_container}>
+            <Image style={Style.image} source={require('../assets/images/head.png')} />
+							<View style={Style.column_container}>
+								<Text style={Style.content}>Series Name:</Text>
+								<Text style={Style.content}>{child.name}</Text>
+            		<Text style={Style.content}>Century date:</Text>
+								<Text style={Style.content}>{child.year}</Text>
+						</View>
+          </View>
+        ))
+      }    
+    </View>
+  );
 }
 
 export default ListHolderComponent;
--- src/styles/listholder.js
+++ src/styles/listholder.js
@@ -7,10 +7,24 @@ export default StyleSheet.create({
     paddingLeft:20, 
     paddingBottom: 10
   },
+  column_container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start"
+  },
+  row_container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-start"
+  },
   title: {
     fontSize: 20
   },
   content: {
     fontSize: 14
+  },
+  image: {
+    width: 50,
+    height: 100
   }
 });
